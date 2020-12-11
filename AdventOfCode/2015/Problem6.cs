f3e2f452bcbbcb30beffd1bb289d2eaea77b0aaf
Kyle Ingraham <kyle.ingraham@ukg.com>
2020-12-11 09:13:05 -0500

    2020 - Day 11, 2015, Day 6

---
+++ AdventOfCode/2015/Problem6.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode._2015
+{
+    public class Problem6
+    {
+        public static void Part1()
+        {
+            var lights = new bool[1000, 1000];
+
+            void TurnOn(int x, int y, int x2, int y2)
+            {
+                for (int i = x; i <= x2; i++)
+                {
+                    for (int j = y; j <= y2; j++)
+                    {
+                        lights[i, j] = true;
+                    }
+                }
+            }
+
+            void TurnOff(int x, int y, int x2, int y2)
+            {
+                for (int i = x; i <= x2; i++)
+                {
+                    for (int j = y; j <= y2; j++)
+                    {
+                        lights[i, j] = false;
+                    }
+                }
+            }
+
+            void Toggle(int x, int y, int x2, int y2)
+            {
+                for (int i = x; i <= x2; i++)
+                {
+                    for (int j = y; j <= y2; j++)
+                    {
+                        lights[i, j] = !lights[i, j];
+                    }
+                }
+            }
+            
+            foreach (var instruction in Helpers.GetInput())
+            {
+                bool turnOn = false;
+                bool toggle = false;
+                bool turnOff = false;
+
+                var copyInstruction = instruction;
+                if (copyInstruction.StartsWith("turn on"))
+                    turnOn = true;
+                else if (copyInstruction.StartsWith("toggle"))
+                    toggle = true;
+                else if (copyInstruction.StartsWith("turn off"))
+                    turnOff = true;
+
+                var match = Regex.Match(copyInstruction, @"([\d]+,[\d]+) through ([\d]+,[\d]+)");
+                var coords1 = match.Groups[1].Value.Split(',').Select(a => Convert.ToInt32(a)).ToArray();
+                var coords2 = match.Groups[2].Value.Split(',').Select(a => Convert.ToInt32(a)).ToArray();
+
+                if (turnOn)
+                    TurnOn(coords1[0], coords1[1], coords2[0], coords2[1]);
+                if (toggle)
+                    Toggle(coords1[0], coords1[1], coords2[0], coords2[1]);
+                if (turnOff)
+                    TurnOff(coords1[0], coords1[1], coords2[0], coords2[1]);
+            }
+
+            int count = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                for (int j = 0; j < 1000; j++)
+                {
+                    count += lights[i, j] ? 1 : 0;
+                }
+            }
+            Console.WriteLine(count);
+        }
+
+        public static void Part2()
+        {
+            var lights = new int[1000, 1000];
+
+            void TurnOn(int x, int y, int x2, int y2)
+            {
+                for (int i = x; i <= x2; i++)
+                {
+                    for (int j = y; j <= y2; j++)
+                    {
+                        lights[i, j] += 1;
+                    }
+                }
+            }
+
+            void TurnOff(int x, int y, int x2, int y2)
+            {
+                for (int i = x; i <= x2; i++)
+                {
+                    for (int j = y; j <= y2; j++)
+                    {
+                        lights[i, j] -= 1;
+                        if (lights[i, j] < 0)
+                            lights[i, j] = 0;
+                    }
+                }
+            }
+
+            void Toggle(int x, int y, int x2, int y2)
+            {
+                for (int i = x; i <= x2; i++)
+                {
+                    for (int j = y; j <= y2; j++)
+                    {
+                        lights[i, j] += 2;
+                    }
+                }
+            }
+
+            foreach (var instruction in Helpers.GetInput())
+            {
+                bool turnOn = false;
+                bool toggle = false;
+                bool turnOff = false;
+
+                var copyInstruction = instruction;
+                if (copyInstruction.StartsWith("turn on"))
+                    turnOn = true;
+                else if (copyInstruction.StartsWith("toggle"))
+                    toggle = true;
+                else if (copyInstruction.StartsWith("turn off"))
+                    turnOff = true;
+
+                var match = Regex.Match(copyInstruction, @"([\d]+,[\d]+) through ([\d]+,[\d]+)");
+                var coords1 = match.Groups[1].Value.Split(',').Select(a => Convert.ToInt32(a)).ToArray();
+                var coords2 = match.Groups[2].Value.Split(',').Select(a => Convert.ToInt32(a)).ToArray();
+
+                if (turnOn)
+                    TurnOn(coords1[0], coords1[1], coords2[0], coords2[1]);
+                if (toggle)
+                    Toggle(coords1[0], coords1[1], coords2[0], coords2[1]);
+                if (turnOff)
+                    TurnOff(coords1[0], coords1[1], coords2[0], coords2[1]);
+            }
+
+            int count = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                for (int j = 0; j < 1000; j++)
+                {
+                    count += lights[i, j];
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ AdventOfCode/2020/Problem11.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode._2020
+{
+    public class Problem11
+    {
+        public const byte EMPTY = 2;
+        public const byte OCCUPIED = 1;
+
+        public static void Part1()
+        {            
+            var input = Helpers.GetInput();
+
+            int maxX = input[0].Length;
+            int maxY = input.Length;
+
+            var layout = new byte[maxY, maxX];
+
+            bool change = false;
+
+            for (int y = 0; y < maxY; y++)
+            {
+                for (int x = 0; x < maxX; x++)
+                {
+                    if (input[y][x] == '#')
+                        layout[y,x] = OCCUPIED;
+                    else if (input[y][x] == 'L')
+                        layout[y, x] = EMPTY;
+                }
+            }
+
+            int occupied = 0;
+            do
+            {
+                change = false;
+                occupied = 0;
+                
+                var newlayout = new byte[maxY, maxX];
+
+                for (int y = 0; y < maxY; y++)
+                {
+                    for (int x = 0; x < maxX; x++)
+                    {
+                        if (layout[y, x] != 0)
+                        {
+                            int adj = 0;
+                            var curState = layout[y, x];
+
+                            // top row
+                            if (y > 0 && x > 0 && layout[y - 1, x - 1] == 1)
+                                adj++;
+                            if (y > 0 && layout[y - 1, x] == 1)
+                                adj++;
+                            if (y > 0 && x < maxX - 1 && layout[y - 1, x + 1] == 1)
+                                adj++;
+                            if (x > 0 && layout[y, x - 1] == 1)
+                                adj++;
+                            if (x < maxX - 1 && layout[y, x + 1] == 1)
+                                adj++;
+                            if (y < maxY - 1 && x > 0 && layout[y + 1, x - 1] == 1)
+                                adj++;
+                            if (y < maxY - 1 && layout[y + 1, x] == 1)
+                                adj++;
+                            if (y < maxY - 1 && x < maxX - 1 && layout[y + 1, x + 1] == 1)
+                                adj++;
+
+                            newlayout[y, x] = layout[y, x];
+
+                            if (adj >= 4)
+                                newlayout[y, x] = EMPTY;
+                            else if (adj == 0)                            
+                                newlayout[y, x] = OCCUPIED;                                                            
+
+                            if (newlayout[y, x] == OCCUPIED)
+                                occupied++;
+
+                            var propState = newlayout[y, x];
+
+                            change = change || curState != propState;
+                        }
+                    }
+                }
+
+                layout = newlayout;
+
+            } while (change == true);
+
+            Console.WriteLine(occupied);
+        }
+        
+        public static void Part2()
+        {
+            var input = Helpers.GetInput();
+
+            int maxX = input[0].Length;
+            int maxY = input.Length;
+
+            var layout = new byte[maxY, maxX];
+
+            bool change = false;
+
+            for (int y = 0; y < maxY; y++)
+            {
+                for (int x = 0; x < maxX; x++)
+                {
+                    if (input[y][x] == '#')
+                        layout[y, x] = OCCUPIED;
+                    else if (input[y][x] == 'L')
+                        layout[y, x] = EMPTY;
+                }
+            }
+
+            int occupied = 0;
+            do
+            {
+                change = false;
+                occupied = 0;
+
+                var newlayout = new byte[maxY, maxX];
+
+                bool SeesOccupied(int y, int x, int dy, int dx)
+                {
+                    while (true)
+                    {
+                        y += dy;
+                        x += dx;
+
+                        if (x < 0 || y < 0 || x >= maxX || y >= maxY)
+                            return false;
+
+                        if (layout[y, x] == OCCUPIED)
+                            return true;
+                        else if (layout[y, x] == EMPTY)
+                            return false;
+                    }
+                }
+
+                for (int y = 0; y < maxY; y++)
+                {
+                    for (int x = 0; x < maxX; x++)
+                    {
+                         if (layout[y, x] != 0)
+                        {
+                            int adj = 0;
+                            var curState = layout[y, x];
+
+                            // top row
+                            if (SeesOccupied(y, x, -1, -1))
+                                adj++;
+                            if (SeesOccupied(y, x, -1, 0))
+                                adj++;
+                            if (SeesOccupied(y, x, -1, 1))
+                                adj++;
+                            if (SeesOccupied(y, x, 0, -1))
+                                adj++;
+                            if (SeesOccupied(y, x, 0, 1))
+                                adj++;
+                            if (SeesOccupied(y, x, 1, -1))
+                                adj++;
+                            if (SeesOccupied(y, x, 1, 0))
+                                adj++;
+                            if (SeesOccupied(y, x, 1, 1))
+                                adj++;
+
+                            newlayout[y, x] = layout[y, x];
+
+                            if (adj >= 5)
+                                newlayout[y, x] = EMPTY;
+                            else if (adj == 0)
+                                newlayout[y, x] = OCCUPIED;
+
+                            if (newlayout[y, x] == OCCUPIED)
+                                occupied++;
+
+                            var propState = newlayout[y, x];
+
+                            change = change || curState != propState;
+                        }
+                    }
+                }
+
+                layout = newlayout;
+
+            } while (change == true);
+
+            Console.WriteLine(occupied);
+        }
+    }
+}
